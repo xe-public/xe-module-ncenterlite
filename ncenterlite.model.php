c65e802932f7f7ef402c091b57e930d87c244f48
BJRambo <sosifam@070805.co.kr>
2014-07-23 02:47:21 +0900

    #9 if문 문제점 개선.

--- ncenterlite.model.php
+++ ncenterlite.model.php
@@ -76,7 +76,11 @@ class ncenterliteModel extends ncenterlite
 				case 'P':
 					$str = sprintf('<strong>%s</strong>님이 <strong>%s</strong>게시판에 <strong>%s</strong>글을 남겼습니다.', $target_member, $v->target_browser, $v->target_summary);
 				break;
-				if($config->android_format === 'mobileplus')
+			}
+
+			if($config->android_format === 'mobileplus')
+			{
+				switch($v->target_type)
 				{
 					case 'S':
 						$oDocumentModel = getModel('document');
