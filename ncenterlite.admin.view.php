ee5883cab2247ae9c7465330eb47816de290f393
BJRambo <sosifam@070805.co.kr>
2014-10-14 05:38:16 +0900

    #62 사용자정보 수집 기능 추가 - sosifam 에서 수집

--- ncenterlite.admin.view.php
+++ ncenterlite.admin.view.php
@@ -43,6 +43,24 @@ class ncenterliteAdminView extends ncenterlite
 
 		Context::set('mid_list', $mid_list);
 
+		// 사용환경정보 전송 확인
+		$ncenterlite_module_info = $oModuleModel->getModuleInfoXml('ncenterlite');
+		$agreement_file = FileHandler::getRealPath(sprintf('%s%s.txt', './files/ncenterlite/', $ncenterlite_module_info->version));
+
+		if(file_exists($agreement_file))
+		{
+			$agreement = FileHandler::readFile($agreement_file);
+			Context::set('_ncenterlite_env_agreement', $agreement);
+			if($agreement == 'Y')
+			{
+				$_ncenterlite_iframe_url = 'http://www.sosifam.com/index.php?mid=ncenterlite_iframe';
+				$_host_info = urlencode($_SERVER['HTTP_HOST']) . '-NC' . $ncenterlite_module_info->version . '-PHP' . phpversion() . '-XE' . __XE_VERSION__;
+				Context::set('_ncenterlite_iframe_url', $_ncenterlite_iframe_url . '&_host='. $_host_info);
+				Context::set('ncenterlite_module_info', $ncenterlite_module_info);
+			}
+		}
+		else Context::set('_ncenterlite_env_agreement', 'NULL');
+
 	}
 
 	function dispNcenterliteAdminList()
