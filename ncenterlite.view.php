6c88e507089c24d882ad3fcae3aeb5fcb1a2a956
BJRambo <sosifam@070805.co.kr>
2014-12-05 19:33:27 +0900

    Revert "#66 회원 알림 정보에서 알림설정시 오류 메세지가 뜨는 문제점 개선"
    This reverts commit bb98e3b9c49e3a1713e220dc2c9f7b844e2db9bf.

--- ncenterlite.view.php
+++ ncenterlite.view.php
@@ -43,15 +43,15 @@ class ncenterliteView extends ncenterlite
 	function dispNcenterliteUserConfig()
 	{
 		$oMemberModel = getModel('member');
-		$member_srl = Context::get('member_srl');
 		$logged_info = Context::get('logged_info');
 		if(!$logged_info) return new Object(-1, '로그인 사용자만 접근할 수 있습니다.');
 
 		if($logged_info->is_admin == 'Y')
 		{
+			$member_srl = Context::get('member_srl');
 			$member_info = $oMemberModel->getMemberInfoByMemberSrl($member_srl);
 		}
-		if($logged_info->is_admin != 'Y' && $logged_info->member_srl != $member_srl)
+		if($logged_info->is_admin != 'Y' && Context::get('member_srl'))
 		{
 			return new Object(-1, '다른회원의 설정을 볼 권한이 없습니다.');
 		}
