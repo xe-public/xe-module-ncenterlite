4cd134055bdd54ead04f6c6b5108be983064a3e8
BJRambo <sosifam@070805.co.kr>
2015-01-01 06:14:06 +0900

    Revert "1차 코드 수정"
    This reverts commit a259f33a283122d09905d4d412c9728504c6c8a5.

--- ncenterlite.admin.model.php
+++ ncenterlite.admin.model.php
@@ -22,34 +22,34 @@ class ncenterliteAdminModel extends ncenterlite
 
 			switch($v->type)
 			{
-				case 'DOCUMENT':
+				case 'D':
 					$type = $lang->ncenterlite_document; //$type = '글';
 				break;
-				case 'COMMENT':
+				case 'C':
 					$type = $lang->ncenterlite_comment; //$type = '댓글';
 				break;
 				// 메시지. 쪽지
-				case 'MESSAGE':
+				case 'E':
 					$type = $lang->ncenterlite_type_message; //$type = '쪽지';
 				break;
 			}
 
 			switch($v->target_type)
 			{
-				case 'COMMENT':
+				case 'C':
 					$str = sprintf($lang->ncenterlite_commented, $target_member, $type, $v->target_summary);
 					//$str = sprintf('<strong>%s</strong>님이 회원님의 %s에 <strong>"%s" 댓글</strong>을 남겼습니다.', $target_member, $type, $v->target_summary);
 				break;
-				case 'ADMINCOMMENT':
+				case 'A':
 					$str = sprintf('<strong>%s</strong>님이 <strong>"%s"</strong>게시판에 <strong>"%s"</strong>댓글을 남겼습니다. ', $target_member, $v->target_browser, $v->target_summary);
 					//$str = sprintf('<strong>%s</strong>님이 회원님의 %s에 <strong>"%s" 댓글</strong>을 남겼습니다.', $target_member, $type, $v->target_summary);
 				break;
-				case 'MENTION':
+				case 'M':
 					$str = sprintf($lang->ncenterlite_mentioned, $target_member,  $v->target_summary, $type);
 					//$str = sprintf('<strong>%s</strong>님이 <strong>"%s" %s</strong>에서 회원님을 언급하였습니다.', $target_member,  $v->target_summary, $type);
 				break;
 				// 메시지. 쪽지
-				case 'MESSAGE':
+				case 'E':
 					if(version_compare(__XE_VERSION__, '1.7.4', '>='))
 					{
 						$str = sprintf('<strong>%s</strong>님께서 <strong>%s</strong> 메세지를 보내셨습니다.',$target_member, $v->target_summary);
@@ -59,10 +59,10 @@ class ncenterliteAdminModel extends ncenterlite
 						$str = sprintf($lang->ncenterlite_message_string, $v->target_summary);
 					}
 				break;
-				case 'TEST':
+				case 'T':
 					$str = sprintf('<strong>%s</strong>님! 스킨 테스트 알림을 완료했습니다.', $target_member);
 				break;
-				case 'DOCUMENTS':
+				case 'P':
 					$str = sprintf('<strong>%s</strong>님이 <strong>"%s"</strong>게시판에 <strong>%s</strong>글을 남겼습니다.', $target_member, $v->target_browser, $v->target_summary);
 				break;
 				case 'S':
@@ -75,7 +75,7 @@ class ncenterliteAdminModel extends ncenterlite
 						$str = sprintf('<strong>%s</strong>님이 <strong>"%s"</strong>글을 남겼습니다.', $target_member, $v->target_summary);
 					}
 				break;
-				case 'VOTED':
+				case 'V':
 					$str = sprintf('<strong>%s</strong>님이 <strong>"%s"</strong>글을 추천하였습니다.', $target_member, $v->target_summary);
 				break;
 			}
